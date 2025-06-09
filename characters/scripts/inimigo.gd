febdaf53b618a3782816e9018b73bdff6edc2db2
Davi Carreiro <carreirodavi370@gmail.com>
2025-06-09 10:54:58 -0300

    inimigo bugado

--- characters/scripts/inimigo.gd
+++ characters/scripts/inimigo.gd
@@ -1,26 +1,27 @@
 extends CharacterBody2D
 
-@export var speed : float = 20.0
+@export var speed: float = 20.0
 @export var intervalo_ataque: float = 2.0
+@export var dano: int = 2
+@export var vida: int = 10
 
 var player: CharacterBody2D = null
-var vida: int = 10 
-var dano: int = 2
-var pode_atacar = true
+var pode_atacar := true
 
 func _physics_process(delta: float) -> void:
-	if player != null and player.is_inside_tree():
+	if player and player.is_inside_tree():
 		var direction = (player.global_position - global_position).normalized()
 		velocity = direction * speed
-		move_and_slide(velocity)
 	else:
 		velocity = Vector2.ZERO
-		move_and_slide(velocity)
+	
+	move_and_slide()
 
-func _on_detection_body_entered(body: Node2D) -> void:
+
+func _on_detection_area_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
-		player = body
+		player = body as CharacterBody2D
 
-func _on_detection_body_exited(body: Node2D) -> void:
+func _on_detection_area_body_exited(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		player = null
