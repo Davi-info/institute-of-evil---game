6bbd11ca2d387432ef4fde4e804f7fa8164df463
Davi Carreiro <carreirodavi370@gmail.com>
2025-06-27 15:14:01 -0300

    tela de pause

--- characters/scripts/GameState.gd
+++ characters/scripts/GameState.gd
@@ -3,3 +3,8 @@ extends Node
 var tentativas: int = 5
 var fase_atual: String = "fase1"
 #var accs_coletadas: int = 0
+
+func reset() -> void:
+	tentativas = 5
+#	accs_coletadas = 0
+	fase_atual = "fase1"
---
+++ characters/scripts/pause_menu.gd
@@ -0,0 +1,24 @@
+extends CanvasLayer
+
+@onready var resume_btn: Button = $menu_holder/resume_btn
+
+func _ready():
+	visible = false
+	
+func _process(delta: float):
+	pass
+
+	
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel"):
+		visible = true
+		get_tree().paused = true
+		resume_btn.grab_focus()
+
+func _on_resume_btn_pressed() -> void:
+	get_tree().paused = false
+	visible = false
+
+
+func _on_menu_prin_btn_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://interface/main_menu.tscn")
