34ed24b020d4ffaf3d8e5652a6f45738cb86097e
Nijoww <tiotony61@gmail.com>
2025-06-30 12:27:33 -0300

    Removendo arquivos duplicados do terrain

--- terrain/characters/scripts/level_2.gd
+++
@@ -1,4 +0,0 @@
-extends Node2D
-
-func _ready() -> void:
-	GameState.fase_atual = "fase2"  # ou "fase2", "fase3" conforme a cena
--- terrain/interface/audio_stream_player_2d.gd
+++
@@ -1,5 +0,0 @@
-extends AudioStreamPlayer2D
-
-func _on_finished() -> void:
-	$".".play()
-	pass # Replace wit
