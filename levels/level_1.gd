47b8759fd34d5cf3fb1d4cbbefa91896e1b0f594
Davi Carreiro <carreirodavi370@gmail.com>
2025-07-02 14:54:28 -0300

    tentanto adicionar cena

--- levels/level_1.gd
+++ levels/level_1.gd
@@ -10,3 +10,13 @@ func _ready() -> void:
 	var hud = hud_scene.instantiate()
 	hud.name = "HUD"
 	add_child(hud)
+
+func _unhandled_input(event: InputEvent) -> void:
+	# Verifica se um diálogo já está em execução
+	if Dialogic.current_timeline != null:
+		return
+
+	# Verifica se a tecla Enter foi pressionada
+	if event is InputEventKey and event.pressed and event.keycode == KEY_ENTER:
+		Dialogic.start('timeline')
+		get_viewport().set_input_as_handled()
