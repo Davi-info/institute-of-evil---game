ab4b1c40ccbddbe9aef13de671bdde051beaeb64
IsaquePFerreira <92412320+IsaquePFerreira@users.noreply.github.com>
2025-07-03 19:03:37 -0300

    Cria interesão

---
+++ characters/scripts/quadro.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+@export var timeline_path := "res://Timeline/quadro.dtl"
+
+var player_na_area := false
+
+func _ready() -> void:
+	Dialogic.start(timeline_path)
+	
+func _process(delta: float) -> void:
+	if player_na_area and Input.is_action_just_pressed("interact"):
+		Dialogic.star(timeline_path)
+
+func _on_body_entered(body):
+	if body.name == "Player":
+		player_na_area = true
+		print("Player entrou")
+
+func _on_body_exited(body):
+	if body.name == "Player":
+		player_na_area = false
+		print("Player saiu")
