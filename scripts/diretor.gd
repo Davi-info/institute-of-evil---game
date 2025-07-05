1c83dce4fdc8bc8eb7f4ddfddd6c525f26385c05
IsaquePFerreira <92412320+IsaquePFerreira@users.noreply.github.com>
2025-07-05 11:26:54 -0300

    Fix Diretor animation

--- scripts/diretor.gd
+++ scripts/diretor.gd
@@ -1,7 +1,4 @@
 extends CharacterBody2D
 
-@onready var animation_player = $AnimationPlayer
-@onready var animated_sprite = $AnimatedSprite2D
-
 func _ready():
-	$AnimationPlayer.play("Diretor_preso")
+	$AnimationPlayer.play("imprisoned_director")
