72b672380777264d8eb181123eba96f504ade253
IsaquePFerreira <92412320+IsaquePFerreira@users.noreply.github.com>
2025-07-06 16:14:45 -0300

    Cria dialogo da porta

--- puzzles/ExitDoor.gd
+++ puzzles/ExitDoor.gd
@@ -1,14 +1,20 @@
 # ExitDoor.gd
 extends StaticBody2D
 
+@onready var interact_area = $InteractArea
 @onready var next_level_area = $"../NextLevelArea"
+@onready var player = get_tree().get_first_node_in_group("player") # ou use get_node se souber o caminho
+
+var door_unlocked := false
 
 func _ready():
 	GameManager.all_accs_collected.connect(_on_all_accs_collected)
-	print("ExitDoor: next_level_area encontrado? ", next_level_area != null) # Adicione esta linha
+	interact_area.body_entered.connect(_on_body_entered)
+
 
 func _on_all_accs_collected():
 	print("Todos os ACCs coletados! Abrindo a porta de saída.")
+	door_unlocked = true
 	open_door()
 
 func open_door():
@@ -19,3 +25,13 @@ func open_door():
 		print("ExitDoor: ERRO! next_level_area é nulo. Não foi possível habilitar a área de transição.") # Adicione esta linha
 	queue_free()
 	print("Porta aberta!")
+
+func _on_body_entered(body):
+	if body.name != "Player":
+		return
+
+	if not door_unlocked:
+		print("Jogador tentou sair sem coletar tudo. Mostrando diálogo.")
+		Dialogic.start("warning_not_all_items")  # Nome da timeline no Dialogic
+	else:
+		print("Jogador entrou na porta já desbloqueada.")
