7ae1acb199a35f2b72ee080c661fbdb4c3645ab2
Davi Carreiro <carreirodavi370@gmail.com>
2025-07-11 11:38:47 -0300

    novas interçoes

--- scripts/level_2.gd
+++ scripts/level_2.gd
@@ -2,7 +2,8 @@ extends Node2D
 
 func _ready() -> void:
 	MusicMenu.stop_music()
-	GameState.fase_atual = "fase2"  # ou "fase2", "fase3" conforme a cena
+	GameState.fase_atual = "fase2"
+	Dialogic.start("timfala_fase2")
 	
 	var hud_scene = preload("res://scenes/interface/hud.tscn")
 	var hud = hud_scene.instantiate()
